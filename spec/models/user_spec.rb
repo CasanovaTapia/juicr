2d993029d4f58cc36abf9c7af03347799c93d391
CasanovaTapia <ching@casanovatapia.com>
2014-11-18 15:04:55 +0800

    user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(FactoryGirl.create(:user)).to be_valid
+  end
+
+  it "returns the name as a string of first name and last name" do
+    user = FactoryGirl.create(:user)
+
+    expect(user.name).to eq(user.first_name + " " + user.last_name)
+  end
 end
