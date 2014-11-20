419d68c0efdf3af50bc2ca3b49e4e0f81d23a31d
CasanovaTapia <ching@casanovatapia.com>
2014-11-20 22:13:18 +0800

    Category policy

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,12 @@
 class CategoriesController < ApplicationController
   def index
     @categories = Category.all
+    authorize @categories
   end
 
   def show
     @category = Category.find(params[:id])
+    authorize @category
   end
 
   def users
@@ -13,15 +15,18 @@ class CategoriesController < ApplicationController
 
   def new
     @category = Category.new
+    authorize @category
   end
 
   def create
     @category = Category.new(category_params)
+    authorize @category
     @category.save
     redirect_to asks_path
   end
 
   def destroy
+    authorize @category
     @category.destroy
   end
 
